1cbd8e2d20ff7047745ff82ac0116fdb002800d4
Daniel Schneider <daschne8@gmail.com>
2019-04-18 14:12:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "Daniel Schneider"
+  end
+
+  get '/hometown' do
+    "Baltimore"
+  end
+
+  get 'favorite-song' do
+    'Girl'
+  end
+  
 end
